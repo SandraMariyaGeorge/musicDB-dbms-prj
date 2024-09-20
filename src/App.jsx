ca791286249271be7353f4f66f32ec3dfb4c2294
Sandra Mariya George <sangeorge04@gmail.com>
2024-09-20 13:39:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+export default function App() {
+  return (
+    <div>
+      <h1>Music Streaming Platform</h1>
+      <p>Welcome to the music streaming platform!</p>
+    </div>  
+  )
+}
